f8418a47945a07055c8cacb3e9ba9f2b4005acaa
Alexey S <justam4444@gmail.com>
2025-10-27 21:26:52 +0300

    first templates added

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TempContainer from "./ui/temp-container";
+import templates from "./data/templates";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>ФИО, оставайтесь, пожалуйста, со мной в чате для оперативного решения данного вопроса..</li>
-        </ol>
-        <TempContainer />
+        {templates.map((temp, index) => (
+          <TempContainer key={index} content={temp.content} name={temp.name} />
+        ))}
       </main>
     </div>
   );
--- app/ui/temp-container.tsx
+++ app/ui/temp-container.tsx
@@ -1,12 +1,32 @@
 'use client'
 
 import React, { useState } from "react";
+import  styled  from '@emotion/styled';
 
-export default function TempContainer() {
+interface TempContainerProps {
+  name: string;
+  content: string;
+}
 
-    const [text, setText] = useState(
-        "Add text you want to copy"
-    );
+export default function TempContainer({ name, content }: TempContainerProps) {
+
+  const Button = styled.button<{ isEditing: boolean }>`
+  padding: 32px;
+  background-color: ${props => props.isEditing ? 'red' : 'hotpink'};
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+  &:hover {
+    color: white;
+    cursor: pointer;
+  }
+`
+    const [text, setText] = useState(content);
+
+    const [editBtn, setEditBtn] = useState('edit');
+    const [isEditing, setIsEditing] = useState(false);
 
     const handleCopyClick = async () => {
       const tempElem = document.getElementById('text-area');
@@ -29,9 +49,18 @@ export default function TempContainer() {
       }
     };
 
-    function activeEditor() {
+    function toggleEditor(): void {
       const tempElem = document.getElementById('text-area');
-      tempElem?.removeAttribute('readOnly');
+
+      // const EBtn = document.getElementById('edit-button');
+      tempElem?.toggleAttribute('readOnly');
+      if (!tempElem?.hasAttribute('readOnly')) {
+        setEditBtn('done');
+        setIsEditing(true);
+      } else {
+        setEditBtn('edit');
+        setIsEditing(false);
+      }
     }
 
     return (
@@ -42,19 +71,19 @@ export default function TempContainer() {
           border: "white 2px solid",
         }}
       >
-        <h1>Hallo</h1>
+        <h1>{name}</h1>
         <div className="text-container">
           <textarea
             value={text}
-            id='text-area'
+            id="text-area"
             onChange={(e) => setText(e.target.value)}
             className="temp-text-container"
             onClick={handleCopyClick}
             readOnly
           ></textarea>
-          <button className="edit-btn" onClick={activeEditor}>
-            Edit
-          </button>
+          <Button className="edit-btn" id="edit-button" onClick={toggleEditor} isEditing={isEditing}>
+            {editBtn}
+          </Button>
         </div>
       </div>
     );
